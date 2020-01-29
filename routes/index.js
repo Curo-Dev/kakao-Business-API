738b23af71bc606f03564072de2d5a70391a9bfc
cheongan <smartnation@naver.com>
2020-01-29 18:28:04 +0900

    feat: ⚡️ Parsing Data

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-
+const cheerio = require('cheerio');
+const request = require('request-promise');
 
 router.get('/', function(req, res, next) {  
+
   res.render('index', { title: 'Express' });
 });
 
 
-router.post('/', function(req, res, next) {  
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {    
+  const { id } = req.body;  
+  
+  if(!id) { res.json({ "code" : 100 }); return }  // ID is Not Found
+  
+  const o = {
+    uri: `https://pf.kakao.com/${id}`,
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+  };
+
+  request(o).then(($) => {
+    const parse = $("#mArticle > div:nth-child(2) > div > div.desc_info > em > span")[0].children[0].data;    
+    const count = parseInt(parse.replace(/,/g, ""));                
+    res.json({ "code" : 200 , "count" : count }); // Success
+    return;
+  }).catch((err) => {
+    res.json({ "code" : 101 }); return; // ID is Wrong  
+  })
 });
+
+
 
 module.exports = router;
